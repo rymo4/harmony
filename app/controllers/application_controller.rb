fa9f9324af7b43cf0c6e660fffd8b3802042b19c
Ryder Moody <rydthemoodster@gmail.com>
2011-08-21 23:41:48 -0700

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  before_filter :set_timezone
+
+  def set_timezone
+    # current_user.time_zone #=> 'London'
+    Time.zone = "Eastern Time (US & Canada)"
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harmony::Application.config.secret_token = '2bf9e26b77bd16dcc4193d7f2a1647540eefe3328dd571afad47548a7278eb2507101e633af18ea9abad793f3afac1e1fab00c9652360eecec732c4b5353ecf3'
